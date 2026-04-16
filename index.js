a0c38d3aa3358660f078ee76507aa28114400053
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 07:55:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// Capitalize the first lter f a string
+// Capitalize the first letter of a string
 export function capitalize(str) {
-  if (!str) retur
-  return str.charAt(0).toUpeCa(+ str.slce(1);
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-// Convert a string to ebab-ca
-export function kebabCse(r) 
-  if (!str) return 
-  return st
+// Convert a string to kebab-case
+export function kebabCase(str) {
+  if (!str) return "";
+  return str
     .replace(/\s+/g, "-")
-    .replace(/([a-z])([A-]/g, "$1-$2"
+    .replace(/([a-z])([A-Z])/g, "$1-$2")
     .toLowerCase();
 }
 
-// Reverse a strin
+// Reverse a string
 export function reverse(str) {
   if (!str) return "";
   return str.split("").reverse().join("");

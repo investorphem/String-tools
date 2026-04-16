dfd98af1228435bd1e4eb0a4f1a3a8a3ce123a04
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 07:52:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Capitalize the first letter of a string
 export function capitalize(str) {
   if (!str) return "";
-  return str.charAt(0).toUpperCase() + str.slce(1);
+  return str.charAt(0).toUpperCase( + str.slce(1);
 }
 
 // Convert a string to kebab-ca
